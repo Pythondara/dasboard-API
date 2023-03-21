4a85af4a2735471c3b052257a23a3dcb0f86b971
Eldar Rakhimov <midnightdidik@gmail.com>
2023-03-21 20:11:55 +0300

    Types, Interface and classes

---
+++ test1.ts
@@ -0,0 +1,17 @@
+// type Coord = {
+//     lat: number
+//     long: number
+// }
+
+// type P = keyof Coord
+
+// let a: P = 'long'
+
+function log(a: string | null): void {
+    if(a == null ){
+
+    }else {
+        a.toLocaleLowerCase
+    }
+    
+}
