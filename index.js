5f032df92ed832a60835e084892d4be591c0f891
Eldar Rakhimov <midnightdidik@gmail.com>
2023-03-21 15:14:42 +0300

    Added user routers

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 import express from "express";
+import { userRouter } from "./users/users.js";
 
 const port = 8000;
 const app = express();
 
 app.get("/hello", (req, res) => {
-    res.send("Hey!");
+    // res.send("Hey!");
+    res.end();
 });
+
+app.use("/users", userRouter);
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
+
+// 'hel?lo' = hello , helo
+// 'hel+lo' = hello, helo, helllllllllllllllllllllllo
+// 'hel*lo' = hello, helsdfsdfwewefsdfsdfsdfsdfsdfsfsfsdfslo
+// 'he(la)?lo' = hello, helalo
+// '/.*a$/' = a mean is ending of string example = dfgdfgjfgjdfgdnmfbgdfnma
