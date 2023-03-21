3871d9f5c1e19034af3b7d8c917f3c72e14026f6
Eldar Rakhimov <midnightdidik@gmail.com>
2023-03-21 16:03:01 +0300

    Added users handler

--- index.js
+++ index.js
@@ -4,12 +4,21 @@ import { userRouter } from "./users/users.js";
 const port = 8000;
 const app = express();
 
+app.use((req, res, next) => {
+    console.log("Time ", Date.now());
+    next();
+});
+
 app.get("/hello", (req, res) => {
-    // res.send("Hey!");
-    res.end();
+    throw new Error("!!!Error!!!");
 });
 
 app.use("/users", userRouter);
+
+app.use((err, req, res, next) => {
+    console.log(err.message);
+    res.status(401).send(err.message);
+});
 
 app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
